d846e35264e2d7ef318f24c9e1106e3e878c21d8
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2023-11-18 10:11:27 +0530

    added 18_Nov_2023.cpp

---
+++ 18_Nov_2023.cpp
@@ -0,0 +1,16 @@
+//1838. Frequency of the Most Frequent Element
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+    sort(nums.begin(),nums.end());
+    long i = 0, N = nums.size(), ans = 1, sum = 0;
+    for (int j = 0; j < N; ++j) {
+        sum += nums[j];
+        while ((j - i + 1) * nums[j] - sum > k) sum -= nums[i++];
+        ans = max(ans, j - i + 1);
+    }
+    return ans;
+    }
+};
