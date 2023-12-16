4d7ee3b0bac6424617cabfaeb87a9c5c294f35ac
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2023-12-16 09:32:39 +0530

    added Better Solution

--- DECEMBER_2023/16_Dec_2023.cpp
+++ DECEMBER_2023/16_Dec_2023.cpp
@@ -1,26 +1,41 @@
 //242. Valid Anagram
 #include<bits/stdc++.h>
 using namespace std;
+//*****************Brute Force Solution********************
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         vector<int> hash(26);
+        
+//         if(s.size() != t.size())
+//             return false;
 
+//         for(int i=0;i<s.size();i++){
+//             hash[s[i]-'a']++;
+//         }
+
+//         for(int i=0;i<t.size();i++){
+//             if(hash[t[i]-'a'] == 0){
+//                  return false;
+//             }
+//             hash[t[i]-'a']--;
+//         }
+//         return true;
+//     }
+// };
+//*************Better Solution*********************
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int> hash(26);
-        
+
         if(s.size() != t.size())
             return false;
-
-        for(int i=0;i<s.size();i++){
-            hash[s[i]-'a']++;
-        }
-
-        for(int i=0;i<t.size();i++){
-            if(hash[t[i]-'a'] == 0){
-                 return false;
-            }
-            hash[t[i]-'a']--;
-        }
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        if(s == t)
+           return true;
+        return false;
     }
 };
 
