56ee1bde20fd6c73080218f9958ce685917d2ad8
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2024-05-10 21:10:00 +0530

    added 10_05_2024.cpp

---
+++ MAY_2024/10_May_2024.cpp
@@ -0,0 +1,29 @@
+// 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        map<double, pair<int, int>> mp;
+        for(int i=0; i<arr.size(); i++)
+        {
+            for(int j=i+1; j<arr.size(); j++)
+            {
+                double div = double(arr[i]) / double(arr[j]);
+                mp[div] = {arr[i], arr[j]};
+            }
+        }
+        int ind = 1;
+        for(auto it : mp)
+        {
+            if(ind == k)
+            {
+                return {it.second.first, it.second.second};
+            }
+            ind++;
+        }
+        return {-1, -1};
+    }
+};
