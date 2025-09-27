e63cbdf5b69da0d8407abd941b1d9e886bda1cc3
Babita Kumari <babitakumari.ky3007@gmail.com>
2025-09-27 11:14:59 +0530

    added 27_sep_2025.cpp and 976.cpp

---
+++ SEPTEMBER_2025/976.cpp
@@ -0,0 +1,18 @@
+// 976. Largest Perimeter Triangle
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for (int k = n - 1; k >= 2; k--) {
+            if (nums[k-2] + nums[k-1] > nums[k]) {
+                return nums[k-2] + nums[k-1] + nums[k];
+            }
+        }
+        return 0;
+    }
+};
