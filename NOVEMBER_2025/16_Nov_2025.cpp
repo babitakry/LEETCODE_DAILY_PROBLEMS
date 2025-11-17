4c92cf5a1f0bae687e42c612fc59fc092bb875ef
Babita Kumari <babitakumari.ky3007@gmail.com>
2025-11-17 18:56:31 +0530

    added 16_Nov_2025.cpp

---
+++ NOVEMBER_2025/16_Nov_2025.cpp
@@ -0,0 +1,29 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+    long long possibleStringCount(long long n) {
+        return (n * (n + 1)) / 2;  
+    }
+
+public:
+    int numSub(string s) {
+        long long MOD = 1e9 + 7;
+
+        long long cnt = 0;
+        long long ans = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '1') {     
+                cnt++;
+            } else {
+                ans = (ans + possibleStringCount(cnt)) % MOD;
+                cnt = 0;
+            }
+        }
+
+        ans = (ans + possibleStringCount(cnt)) % MOD; 
+
+        return (int)ans;
+    }
+};
