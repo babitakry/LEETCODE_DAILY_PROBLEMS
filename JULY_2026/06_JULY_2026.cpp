7e113e58b6d1624b8341fcd79476bfa66b842965
Babita Kumari <babitakumari.ky3007@gmail.com>
2026-07-06 17:20:21 +0530

    add solution for problem 1288: Remove Covered Intervals

---
+++ JULY_2026/06_JULY_2026.cpp
@@ -0,0 +1,29 @@
+// 1288. Remove Covered Intervals
+
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            int a = intervals[i][0];
+            int b = intervals[i][1];
+
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+
+                int c = intervals[j][0];
+                int d = intervals[j][1];
+
+                if (c <= a && b <= d) {
+                    cnt++;
+                    break;                  
+                }
+            }
+        }
+
+        return n - cnt;
+    }
+};
