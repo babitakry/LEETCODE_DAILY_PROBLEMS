d9aa0553f807457f9ffa8d87360cd9cd693be8a7
Babita Kumari <babitakumari.ky3007@gmail.com>
2026-07-24 22:15:31 +0530

    add solution for problem 3514: Number of Unique XOR Triplets II

---
+++ JULY_2026/23_JULY_2026.cpp
@@ -0,0 +1,18 @@
+// 3514. Number of Unique XOR Triplets II
+
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n == 1 || n == 2)
+            return n;
+        
+        int ans = 1; //2^0;
+        while(ans <= n) {
+            ans = (ans << 1);
+        }
+
+        return ans;
+    }
+};
