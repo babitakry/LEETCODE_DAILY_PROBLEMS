46b3582e47e1212addea65d08cdd79d7bf1e3400
Babita Kumari <babitakumari.ky3007@gmail.com>
2026-07-28 07:43:56 +0530

    add solution for problem 3517: Smallest Palindromic Rearrangement I

---
+++ JULY_2026/28_JULY_2026.cpp
@@ -0,0 +1,15 @@
+// 3517. Smallest Palindromic Rearrangement I
+
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n = s.size();
+        int mid = n / 2;
+
+        sort(s.begin(), s.begin()+mid);
+        for(int i = 0; i < mid; i++){
+            s[n-i-1] = s[i];
+        }
+        return s;
+    }
+};
