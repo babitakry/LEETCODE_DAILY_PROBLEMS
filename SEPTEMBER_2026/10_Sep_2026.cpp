52114aa4915a726d1b68347f5e75ef012ab4d726
Babita Kumari <babitakumari.ky3007@gmail.com>
2026-09-10 21:54:04 +0530

    add solution for problem 3904: Average of Subtree

---
+++ SEPTEMBER_2026/10_Sep_2026.cpp
@@ -0,0 +1,36 @@
+#include<vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    pair<int,int> countAvgSubTree(TreeNode* root, int &count){
+        if(root == NULL)
+            return {0, 0};
+        
+        auto left =  countAvgSubTree(root->left, count);
+        auto right = countAvgSubTree(root->right, count);
+
+        int sum = left.first + right.first + root->val;
+        int n = left.second + right.second + 1;
+
+        if(sum / n == root->val){
+            count++;
+        }
+
+        return {sum, n};
+    }
+public:
+    int averageOfSubtree(TreeNode* root) {
+        int count = 0;
+        countAvgSubTree(root, count);
+        return count;
+    }
+};
